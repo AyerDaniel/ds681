29dbaf3226e4bdc9250ab5aa08a73e62117dc09a
Daniel Ayer <da632@njit.edu>
2026-05-12 13:58:43 -0400

    Reorganized file structure.

--- eng-ai-agents-main/assignments/assignment-3/training_metrics.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/home/johnsmith/Desktop/njit/workspaces/ds681/eng-ai-agents-main/assignments/assignment-3/runs/yolo-drone-20260306-185910/results.csv")
-df.columns = df.columns.str.strip()
-df["F1"] = 2 * (df["metrics/precision(B)"] * df["metrics/recall(B)"]) / \
-                (df["metrics/precision(B)"] + df["metrics/recall(B)"])
-print(df[["epoch", "metrics/precision(B)", "metrics/recall(B)", "metrics/mAP50(B)", "F1"]])
